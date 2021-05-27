943e304e7351bf748ad62c3344f997b1a4acbad0
Themenestrel <50584077+Themenestrel@users.noreply.github.com>
2021-05-27 17:26:20 -0300

    Atualização Falta '"complete"

--- to_do_novo/todo_novo_app/urls.py
+++ to_do_novo/todo_novo_app/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 from .models import Tarefa, Grupos, Sub_Grupos
-from .views import Logar, Registrar, ListaTarefa, TodasTarefas, VisualizaGrupo, VisualizaSubGrupo, CriarGrupo, AtualizarGrupo, \
+from .views import Logar, Registrar, VisualizaGrupo, VisualizaSubGrupo, CriarGrupo, AtualizarGrupo, \
     ApagarGrupo, MostraSubGrupo, pega_get, pega_get_tarefa, exclui_subgrupo, edita_subgrupo, exclui_tarefa, edita_tarefa
 from django.contrib.auth.views import LogoutView
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('tarefas/<int:pk>', ListaTarefa.as_view(), name='tarefas'),
     path('login/', Logar.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('registrar/', Registrar.as_view(), name='registrar'),
-    path('todas/', TodasTarefas.as_view(), name='completo'),
     path('', VisualizaGrupo.as_view(), name='grupos'),
     path('subgrupos/<int:pk>', VisualizaSubGrupo.as_view(), name='subgrupos'),
     path('criar/<int:pk>', pega_get_tarefa, name='criar'),
